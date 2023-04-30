4524a98a17031d28bdb4663c2effcf70f0d28958
meretska-sofiia <meretska.sofiia@gmail.com>
2023-04-30 21:14:06 +0300

    update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose
   .then(() => {
     console.log("Database connection successful");
     app.listen(PORT, () => {
-      console.log("Server running. Use our API on port: ", PORT);
+      console.log("Server running. Use our API on port: ", PORT || 3000);
     });
   })
   .catch((error) => {
