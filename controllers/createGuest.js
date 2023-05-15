8a4b433744a20160f7cd177e9039325556915c4c
meretska-sofiia <meretska.sofiia@gmail.com>
2023-05-15 11:38:17 +0300

    Update createGuest.js

--- controllers/createGuest.js
+++ controllers/createGuest.js
@@ -3,6 +3,9 @@ const { Guest } = require("../models/guest");
 const getGuestName = async (req, res) => {
   const { userId } = req.query;
   const guest = await Guest.findById(userId);
+  if (!userId) {
+    res.status(404).json({ message: "Guest not found" });
+  }
   if (!guest) {
     res.status(404).json({ message: "Guest not found" });
   } else {
